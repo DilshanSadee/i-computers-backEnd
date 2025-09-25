0d0649e5c7dc87659f78769c88d32b58e3deabe7
DilshanSadee <dilshansadee2525@gmail.com>
2025-09-25 20:00:49 +0530

    day10

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(express.json())
 app.use("/users",userRouter)
 app.use("/product",productRouter)
 
-app.listen(3000 , 
+app.listen(3000 , +
     ()=>{
         console.log("server is running")
     }
