8be9c50b9ce8739a9b2e9c8850240f90ba3fae91
DilshanSadee <dilshansadee2525@gmail.com>
2025-09-30 21:12:40 +0530

    Day10

---
+++ i-computers-fronend/src/componentes/productCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard(){
+    return(
+              <div>
+           <h1>Laptop</h1>
+        <img src="https://picsum.photos/id/1/200/300" alt="" />
+        <p>Price lkr 100000/= </p>
+      </div>
+    )
+}
