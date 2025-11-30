bcf534c96befbc6b427430fb212aa7be0a7896ac
DilshanSadee <dilshansadee2525@gmail.com>
2025-11-30 20:28:53 +0530

    day23✔

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
+import axios from "axios";
 
 export function createUser(req,res){
 
@@ -14,7 +14,6 @@ export function createUser(req,res){
         firstName : data.firstName,
         lastName : data.lastName,
         password : hashedPassword,
-        role : data.role,
     })
 
     user.save().then(
@@ -79,4 +78,89 @@ export function isAdmin(req){
     }
 
     return true
+}
+
+export function getUser(req, res){
+
+    if (req.user == null){
+        res.status(401).json({
+            message : "unautherized"
+        })
+        return
+    }
+    res.json(req.user)
+
+}
+
+export async function googleLogin(req,res){
+
+    console.log(req.body.token)
+
+    try{
+        const response = await axios.get("https://www.googleapis.com/oauth2/v1/userinfo",{
+            headers :  {
+                Authorization : `Bearer ${req.body.token}`
+            }
+        })
+
+        console.log(response.data);
+
+        const user = await User.findOne({email : response.data.email})
+
+        if(user == null){
+                const newUser = new User({
+                    email : response.data.email,
+                    firstName: response.data.given_name,
+                    lastName : response.data.family_name,
+                    password : "123",
+                    image : response.data.picture,
+                })
+
+                await newUser.save()
+
+                const payload = {
+                        email : newUser.email,
+                        firstName :  newUser.firstName,
+                        lastName :  newUser.lastName,
+                        role :  newUser.role,
+                        isEmailVerified : true,
+                        image :  newUser.image
+                };
+                 const token = jwt.sign(payload,process.env.JWT_SECRET,
+                    )
+
+                    res.json({
+                        message : "login succesfull",
+                        token : token,
+                        role : newUser.role
+                    })
+        }else{
+            const payload ={
+                        email : user.email,
+                        firstName : user.firstName,
+                        lastName : user.lastName,
+                        role : user.role,
+                        isEmailVerified : user.isEmailVerified,
+                        image : user.image
+                    };
+                    const token = jwt.sign(payload,process.env.JWT_SECRET,
+                    )
+                        
+
+                    res.json({
+                        message: "user succpully logged",
+                        token : token,
+                        role : user.role,
+                    
+                    });
+
+        }
+        
+    }catch(error){
+        res.status(500).json({
+            message : "google login failed",
+            error : error.message
+        })
+    }
+    
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { createUser, loginUser } from "../controllers/userController.js"
+import { createUser, getUser, googleLogin, loginUser } from "../controllers/userController.js"
 import { deleteProduct } from "../controllers/productController.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
-
+userRouter.get("/",getUser)
+userRouter.post("/google-login", googleLogin)
 
 
 export default userRouter
